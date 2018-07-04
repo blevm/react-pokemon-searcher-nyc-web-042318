1f8fd2bba71c31194c0b7e2d61f76a0231b63c4b
blevm <mjblevins7@gmail.com>
2018-07-04 17:27:31 -0400

    initial display wip

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,29 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      sideDisplayed: 'front'
+    }
+  }
+
   render() {
+    let { id, sprites, name, stats } = this.props.pokemon
     return (
-      <Card>
+      <Card key={id}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" src={(this.state.sideDisplayed === 'front') ? sprites.front : sprites.back }/>
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              // {stats.find(stat => stat.name === 'hp').value} hp
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,27 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      pokemon: null
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(`http://localhost:3000/pokemon`).then(resp => resp.json()).then(pokemon => this.setState({pokemon}))
+  }
+
   render() {
+    console.log(`in render`, this.state.pokemon);
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
         <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon}/>
         <br />
         <PokemonForm />
       </div>
